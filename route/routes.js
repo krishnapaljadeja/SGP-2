a3534547e2366990d1c30c3b31bb770743962e7b
krishnapaljadeja <147587699+krishnapaljadeja@users.noreply.github.com>
2025-06-20 22:06:55 +0530

    Replace bcrypt with bcryptjs

--- route/routes.js
+++ route/routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const cookieParser = require("cookie-parser")
 const jwt = require("jsonwebtoken")
 const collection = require("../models/user");
